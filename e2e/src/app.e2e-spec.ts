56108cc95d6f0c5da02dabd2d20cd8bf309d7028
IDea Factory <dev-team@ideafactory.fr>
2019-02-27 14:34:18 +0100

    Intégration des tests et ee

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to navette!');
+    expect(page.getTitleText()).toEqual('Navette Ste - Stex');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,37 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { IhmModule } from './shared/ihm/ihm/ihm.module';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        IhmModule,
+        ToastrModule.forRoot(
+          {
+            timeOut: 3000,
+            positionClass: 'toast-bottom-right',
+            preventDuplicates: true
+          }
+        )
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        ToastrService
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', inject([ToastrService], (service: ToastrService) => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'navette'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('navette');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to navette!');
-  });
+  }));
 });
